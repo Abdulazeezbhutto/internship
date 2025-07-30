7987336aafc4857a6accda3af435f16ef88d7bbc
Azeez <abdulazeezbhutto085@gmail.com>
2025-07-29 22:18:40 -0700

    version-1

---
+++ Blogge-1.0.0.0/index.php
@@ -0,0 +1,26 @@
+<?php
+
+//header start
+include("includes/header.php");
+//header end
+
+
+// nav bar start
+include("includes/navbar.php");
+// nav bar end
+
+
+
+
+//footer start
+include("includes/footer.php");
+//footer end
+
+
+
+
+
+
+
+
+?>
