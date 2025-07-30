aa3e6b9927b57e8f13ba02ad79ce1bf95cce37e4
Abdul Azeez <150655782+Abdulazeezbhutto@users.noreply.github.com>
2025-07-30 04:26:36 -0700

    content added in index.php and admin dashboard added

---
+++ Blogge-1.0.0.0/admin_dashboard.php
@@ -0,0 +1,31 @@
+<?php
+
+//header start
+include("includes/header.php");
+//header end
+
+
+// nav bar start
+include("includes/navbar.php");
+
+// nav bar end
+
+// admin dashbooard start
+?>
+   
+
+
+
+
+
+
+
+
+<?php
+
+// admin dashboard end
+
+
+
+include("includes/footer.php");
+?>
